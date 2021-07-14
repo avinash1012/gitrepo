7194ed67a7dc90e978937f7aaca22825bea15871
avinash1012 <87105887+avinash1012@users.noreply.github.com>
2021-07-14 22:27:48 +0530

    changes made
    changes made

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello child second commit guys");
+System.out.println("hello child changes made second commit guys");
 }
 }
 
